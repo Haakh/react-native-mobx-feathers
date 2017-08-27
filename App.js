da205ca79ff022d9dc163a7f098bb7a57c783058
Bhavish <bhavish@Bhavishs-MacBook-Air.local>
2017-08-27 13:49:43 +0530

    project config

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
-import app from './src/app';
+// import app from './src/app';
 
-export default app;
+// export default app;
+
+export default from './storybook';
--- src/components/login.js
+++ src/components/login.js
@@ -1,34 +1,60 @@
-import React, { Component } from 'react';
-import {
-  PropTypes,
-  View,
-  Text,
-  Button,
-} from 'react-native';
-import { observer, inject } from 'mobx-react/native';
-import { StackNavigator } from 'react-navigation';
+import React, { Component } from "react";
+import { PropTypes, View, Text, Button } from "react-native";
+import { Content, Form, Item, Input, Label, Icon } from "native-base";
+import { observer, inject } from "mobx-react/native";
+import { StackNavigator } from "react-navigation";
 
 export default class Login extends React.Component {
   static navigationOptions = {
-    title: 'Login',
+    title: "Login"
   };
 
-render () {
+  render() {
     const { navigate } = this.props.navigation;
     return (
-    <View>
-        <Text>
-            Hello There.
-        </Text>
-        <Button
-          onPress={() => navigate('Home')}
-          title="Login"
-        />
-        <Button
-          onPress={() => navigate('SignUp')}
-          title="SignUp"
-        />
-    </View>
+      <Content>
+        <View>
+          <Form>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                flex: 1
+              }}
+            >
+              <Item floatingLabel style={{ flex: 9 }}>
+                <Label>Username</Label>
+                <Input />
+              </Item>
+              <View style={{ flex: 1 }}>
+                <View style={{ flex: 5 }} />
+                <Icon style={{ flex: 1 }} active name="ionitron" />
+              </View>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                flex: 1
+              }}
+            >
+              <Item floatingLabel style={{ flex: 9 }}>
+                <Label>Password</Label>
+                <Input />
+              </Item>
+              <View style={{ flex: 1 }}>
+                <View style={{ flex: 5 }} />
+                <Icon style={{ flex: 1 }} active name="finger-print" />
+              </View>
+            </View>
+          </Form>
+
+          <Button onPress={() => navigate("Home")} title="Login" />
+          <Button onPress={() => navigate("SignUp")} title="SignUp" />
+        </View>
+      </Content>
     );
-}
+  }
 }
