11af9b96fca42665f0589ee7c7b3235845e3ec10
Bhavish <bhavish@Bhavishs-MacBook-Air.local>
2017-09-02 23:54:38 +0530

    added async fonts

--- src/boot/react.js
+++ src/boot/react.js
@@ -1,3 +1,4 @@
+import Expo from 'expo';
 import React from 'react';
 import { Provider } from 'mobx-react/native';
 import { AppRegistry, View, BackAndroid, StatusBar } from 'react-native';
@@ -17,7 +18,28 @@ import platform from '../../native-base-theme/variables/platform';
 
 export default function (stores) {
   return class Root extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        isLoading: false,
+        isReady: false,
+      };
+    }
+
+    async componentWillMount() {
+      await Expo.Font.loadAsync({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+        Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf'),
+      });
+      this.setState({ isReady: true });
+    }
+
     render() {
+      if (!this.state.isReady || this.state.isLoading) {
+        return <Expo.AppLoading />;
+      }
+
       return (
         <View style={{ flex: 1 }}>
           <StyleProvider style={getTheme(platform)}>
